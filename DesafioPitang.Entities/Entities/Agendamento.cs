713e775bbd73b18db7f024287852154231c6d954
Guil <Flashnat.br@gmail.com>
2024-07-05 16:24:34 -0300

    [feat] Definir entidades

---
+++ DesafioPitang.Entities/Entities/Agendamento.cs
@@ -0,0 +1,15 @@
+﻿namespace DesafioPitang.Entities.Entities
+{
+    public class Agendamento
+    {
+        public int IdPaciente { get; set; }
+        public DateOnly Data { get; set; }
+        public TimeSpan Hora { get; set; }
+        public string Status { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+        public Paciente Paciente { get; set; }
+
+        public Agendamento() { }
+    }
+}
