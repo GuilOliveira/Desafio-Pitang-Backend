5bffa314bf4214cc4705ed7baff67669d1ef6a61
Guil <Flashnat.br@gmail.com>
2024-07-05 18:49:12 -0300

    [fix]Adicionar o Contexto

---
+++ DesafioPitang.Repository/Context.cs
@@ -0,0 +1,19 @@
+﻿using DesafioPitang.Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioPitang.Repository
+{
+    public class Context : DbContext
+    {
+        public DbSet<Agendamento> Agendamento { get; set; }
+        public DbSet<Paciente> Paciente { get; set; }
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(Context).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
