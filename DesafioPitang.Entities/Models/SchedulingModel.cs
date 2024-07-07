ac8253ad6ec6ae8c09423417d88a6608f2dc5724
Guil <Flashnat.br@gmail.com>
2024-07-07 00:09:45 -0300

    [feat] Adicionar Controllers iniciais

---
+++ DesafioPitang.Entities/Models/SchedulingModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DesafioPitang.Entities.Models
+{
+    public class SchedulingModel
+    {
+        public string PatientName { get; set; }
+        public DateTime PatientBirthDate { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public TimeSpan AppointmentTime { get; set; }
+    }
+}
