132cd1dbf937bfe586416a864e2a56436c92e576
Guil <Flashnat.br@gmail.com>
2024-07-16 18:10:26 -0300

    [feat] Criar entidade do Usuario

--- DesafioPitang.Entities/Entities/Patient.cs
+++ DesafioPitang.Entities/Entities/Patient.cs
@@ -5,8 +5,10 @@
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
         public DateTime CreatedAt { get; set; }
+        public int UserId { get; set; }
 
         public List<Appointment> Appointments { get; set; }
+        public User User { get; set; }
 
         public Patient() { }
     }
