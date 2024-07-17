82d90ecead475c88de02583b41c4f323a4844cce
Guil <Flashnat.br@gmail.com>
2024-07-16 23:40:42 -0300

    [feat] Criar negocio de autenticacao

---
+++ DesafioPitang.Entities/DTOs/UserTokenDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace DesafioPitang.Entities.DTOs
+{
+    public class UserTokenDTO
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+        public UserTokenDTO(string token, string refreshToken)
+        {
+            Token = token;
+            RefreshToken = refreshToken;
+        }
+    }
+}
---
+++ DesafioPitang.Repository/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using DesafioPitang.Entities.Entities;
+using DesafioPitang.Repository.Interface.IRepositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioPitang.Repository.Repositories
+{
+    internal class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(Context context) : base(context)
+        {
+        }
+        public async Task<User> GetByEmail(string email)
+        {
+            return await EntitySet.FirstOrDefaultAsync(user=>user.Email == email);
+        }
+    }
+}
